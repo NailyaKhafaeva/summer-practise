bdd04abf5e1d9af428f616da20a6c7e9a3f5db97
NailyaKhafaeva <54901919+NailyaKhafaeva@users.noreply.github.com>
2021-07-14 12:10:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,12 @@ package com.company.com.company;
 
 import com.company.SimpleGUI;
 import com.company.Edge;
-import com.company.DSF;
+import com.company.DSF
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//import com.mxgraph.swing.mxGraphComponent;
-//import com.mxgraph.view.mxGraph;
-
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 
