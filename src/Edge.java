47d6b9cf42c21786f563c3ea3a8e9449c48739a6
NailyaKhafaeva <54901919+NailyaKhafaeva@users.noreply.github.com>
2021-07-14 14:12:59 +0300

    Update Edge.java

--- src/Edge.java
+++ src/Edge.java
@@ -1,13 +1,19 @@
 package com.company;
 
-public class Edge {
-    public int u;
-    public int v;
-    public int w;
+class Edge implements Comparable<Edge>  {
+    int u;
+    int v;
+    int w;
 
     Edge(int u, int v, int w) {
         this.u = u;
         this.v = v;
         this.w = w;
+    }
+
+    @Override
+    public int compareTo(Edge edge) {
+        if (w != edge.w) return w < edge.w ? -1 : 1;
+        return 0;
     }
 }
